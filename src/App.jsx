303a63526f3733f11c37b482e7038582fceb77da
Michelle Wee <michellew1822@gmail.com>
2025-12-19 20:22:28 -0800

    setting up basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import Homescreen from "./components/Homescreen.jsx";
+import NoPage from "./components/NoPage.jsx";
+
+import {HashRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
 
 function App() {
 
   return (
     <>
+      <HashRouter>
+        <Routes>
+          <Route index element={<Homescreen/>}/>
+            
+          <Route path="/home" element={<Homescreen />}/>
+
+          <Route path="*" element={<NoPage />} />
+        </Routes>
+      </HashRouter>
     </>
   )
 }
---
+++ src/components/Homescreen.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+
+import "./Homescreen.css";
+
+function Homescreen (){
+
+    return (
+
+        <div>  
+            This is the start of a new project.        
+        </div>
+
+    );
+
+}
+
+
+export default Homescreen;
