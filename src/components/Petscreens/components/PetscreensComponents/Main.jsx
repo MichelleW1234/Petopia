947ad585bcc4440c339864d85d69ef3af7f77f34
Michelle Wee <michellew1822@gmail.com>
2026-04-17 02:59:54 -0700

    note

--- src/components/Petscreens/components/PetscreensComponents/Main.jsx
+++ src/components/Petscreens/components/PetscreensComponents/Main.jsx
@@ -150,7 +150,7 @@ function Main ({mainAnimationImages, mainPetEnergy, mainPetMood, mainActivityInP
 
                             <div className= {`MainPetWindowGrid MainPetWindowGrid-${PetList[ActivePetName][speciesKey]}`}>  
 
-                                {/* CHANGE THIS SO THAT HEARTS OR ANNOYANCE SYMBOL SHOWS UP ONLY WHEN PET (WHOLE IMAGE CHANGES) AND DELETE THE HOVERING EMOTION ???????? */}
+                                {/* CHANGE THIS SO THAT HEARTS OR ANNOYANCE SYMBOL SHOWS UP ONLY WHEN PET (OVERLAY PET IMAGE) AND DELETE THE HOVERING EMOTION ???????? */}
                                 {mainInnerScreenSpace.map((row, rowIndex) => (
                                     row.map((__, colIndex) => {
 
