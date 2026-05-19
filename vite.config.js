196c1ff4444b0a67053fcce4c794a27a0216dd04
Michelle Wee <michellew1822@gmail.com>
2026-05-18 19:38:13 -0700

    more electron setup

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',  // Important: make assets relative so Electron can load them
+  build: {
+    outDir: 'dist',
+  },
   server: {
     host: 'localhost',
     port: 4000,
